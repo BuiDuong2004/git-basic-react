a25a57723c1f970e3baa2846f126ca0ef835f5fb
duongbdh <duongbdhe182316@fpt.edu.vn>
2026-01-22 14:51:05 +0700

    Add button tambiet

--- src/App.js
+++ src/App.js
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 function App() {
   const [msg, setMsg] = useState("");
+  const[tambiet,setTambiet]=useState("tạm biệt")  
 
   return (
-    <div>
+    <>
+     <div>
       <button onClick={() => setMsg("xin chào")}>
         Xin chào
       </button>
-
       <p>{msg}</p>
     </div>
+      <div>
+      <button onClick={() => setTambiet("bye bye")}>
+        Tạm biệt
+      </button>
+      <p>{tambiet}</p>
+    </div>
+    </>
+   
   );
 }
 
